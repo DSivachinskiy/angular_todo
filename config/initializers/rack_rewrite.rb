17dc8e63e253ae7b064902f2004455b17ee8ee25
DSivachinskiy <grinjou@inbox.ru>
2015-07-06 15:17:29 +0300

    fix

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,4 +1,4 @@
-RailsAngularBowerHeroku::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+Rails::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
   rewrite %r{/fonts/(.*)}, '/vendor/assets/bower_components/bootstrap/fonts/$1'
   rewrite %r{/font/(.*)}, '/vendor/assets/bower_components/bootstrap/font/$1'
 end
